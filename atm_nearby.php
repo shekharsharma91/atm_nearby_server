f2c9616923da9b3cfc2c61ac4c9bbb450e02d044
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-03-13 01:35:43 +0700

    update atm nearby server

--- atm_nearby.php
+++ atm_nearby.php
@@ -1,21 +1,38 @@
 <?php
 require("lib/koneksi.php");
 
-@$lat= $_GET['lat'];
-@$long = $_GET['long'];
+@$lat= $_GET['lat']; // -6.909013
+@$long = $_GET['long']; //107.625128
+@$sum = $_GET['sum']; //5
+@$nm_atm = $_GET['atm']; //BCA
+@$radius = $_GET['radius']; //25 (KM)
 
 if ($lat && $long) {
-	$query = mysql_query("SELECT id, 
-								 latitude, 
-								 longitude, 
-								 (3959*acos(cos(radians(37)) * cos(radians(latitude)) * cos(radians(longitude) - radians($long)) + sin( radians($lat)) * sin(radians( latitude )) ) ) AS distance
-					FROM atm_data
-					HAVING distance > 35
-					ORDER BY distance");
-	while($data = mysql_fetch_assoc($query)) {
-		$atm_detail[] = $data;
+	if ($sum != "" && $nm_atm != "" && $radius != "") {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis WHERE nm_atm LIKE '%".$nm_atm."%' HAVING distance < ".$radius." ORDER BY distance LIMIT ". $sum;	
+	} else if ($nm_atm != "" && $sum != "" ) {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis WHERE nm_atm LIKE '%".$nm_atm."%' HAVING distance < 25 ORDER BY distance LIMIT ". $sum;
+	} else if ($nm_atm != "" && $radius != "") {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis WHERE nm_atm LIKE '%".$nm_atm."%' HAVING distance < ".$radius." ORDER BY distance LIMIT 5";
+	} else if ($nm_atm != "") {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis WHERE nm_atm LIKE '%".$nm_atm."%' HAVING distance < 25 ORDER BY distance LIMIT 5";
+	} else if ($radius != "") {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis HAVING distance < ".$radius." ORDER BY distance LIMIT 5";
+	} else if ($sum != "") {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis HAVING distance < 25 ORDER BY distance LIMIT ". $sum;
+	} else {
+		$my_query = "SELECT latitude, longitude, ( 6371 * acos( cos( radians(".$lat.") ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(".$long.") ) + sin( radians(".$lat.") ) * sin( radians( latitude ) ) ) ) AS distance, nm_atm, alamat FROM atmgis HAVING distance < 25 ORDER BY distance LIMIT 5";
 	}
-	echo json_encode($atm_detail);
+	
+	$query = mysql_query($my_query);
+	if (mysql_num_rows($query) == 0) {
+		echo '[]';
+	} else {
+		while($data = mysql_fetch_assoc($query)) {
+			$atm_detail[] = $data;
+		}
+		echo json_encode($atm_detail);
+	}
 } else {
 	echo '{"data":"NULL"}';
 }
--- lib/koneksi.php
+++ lib/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$database = "atm-nearby-server";
-$user     = "root";
-$pass = "";		
+$database = "cmlocat1_atm_neaby";
+$user     = "cmlocat1_root";
+$pass = "agusprasetyo811";		
 	
 $link = mysql_connect($host,$user,$pass) or die("Server tidak ditemukan");
 mysql_select_db($database,$link)or die("Database tidak ditemukan");
